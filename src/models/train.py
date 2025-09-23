0ad362d0df00145fb9f12de1ba775636dc5735e2
renatoinomata <renato.inomata@gmail.com>
2025-09-22 21:27:57 -0300

    uv.lock and train.py

---
+++ src/models/train.py
@@ -0,0 +1,9 @@
+params = {
+    'learning_rate': 0.050623168535997125, 
+    'max_iter': 1390, 
+    'max_leaf_nodes': 2260, 
+    'max_depth': 9,
+    'min_samples_leaf': 41, 
+    'l2_regularization': 1.1298645707113427e-07, 
+    'max_bins': 213,
+}
